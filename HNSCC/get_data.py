8522a8a05a986ed01a79e8249c575b4ff2fbd0dd
Lucas McCullum <lumccul3@gmail.com>
2021-03-31 19:22:57 -0400

    Updates data loader

--- HNSCC/get_data.py
+++ HNSCC/get_data.py
@@ -6,195 +6,187 @@ import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
 import pydicom
+from skimage import io, transform
 import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader
 
 
 class SurvivalDataset(Dataset):
-    def __init__(self):
-        # Find a better way to flip some of these images to the correct orientation
-        self.flip_array = [
-            'data/HNSCC-01-0199/10-26-2002-RT SIMULATION-18560/3.000000-58373',
-            'data/HNSCC-01-0018/03-01-2009-RT SIMULATION-16942/10.000000-60812',
-            'data/HNSCC-01-0011/11-11-1997-RT SIMULATION-19590/10.000000-88887',
-            'data/HNSCC-01-0016/03-07-1998-RT SIMULATION-64055/10.000000-15759',
-            'data/HNSCC-01-0086/07-03-1999-RT SIMULATION-42088/92691',
-            'data/HNSCC-01-0017/05-04-1998-RT SIMULATION-38793/10.000000-76074',
-            'data/HNSCC-01-0010/03-30-2009-RT SIMULATION-52386/10.000000-61881',
-            'data/HNSCC-01-0214/07-27-2004-RT SIMULATION-67763/3.000000-43635',
-            'data/HNSCC-01-0004/08-24-1996-RT SIMULATION-72882/10.000000-74926',
-            'data/HNSCC-01-0201/10-21-2002-RT SIMULATION-79781/2.000000-47027',
-            'data/HNSCC-01-0005/01-19-1998-RT SIMULATION-39358/10.000000-79230',
-            'data/HNSCC-01-0208/02-02-2003-RT SIMULATION-27554/6.000000-44043',
-            'data/HNSCC-01-0183/07-01-2002-RT SIMULATION-37569/3.000000-83360',
-            'data/HNSCC-01-0015/02-16-1998-RT SIMULATION-83066/10.000000-53517',
-            'data/HNSCC-01-0012/10-11-1997-RT SIMULATION-86133/10.000000-11509',
-            'data/HNSCC-01-0013/02-23-1998-RT SIMULATION-65901/10.000000-22461',
-            'data/HNSCC-01-0085/09-21-1999-RT SIMULATION-14635/2.000000-33945',
-            'data/HNSCC-01-0007/04-29-1997-RT SIMULATION-32176/10.000000-72029',
-            'data/HNSCC-01-0009/05-14-1997-RT SIMULATION-35793/10.000000-66060',
-            'data/HNSCC-01-0203/12-10-2002-RT SIMULATION-76477/4.000000-83318',
-            'data/HNSCC-01-0008/05-14-1997-RT SIMULATION-54670/10.000000-96097',
-            'data/HNSCC-01-0006/06-09-1997-RT SIMULATION-57410/10.000000-41643',
-            'data/HNSCC-01-0180/05-15-2002-RT SIMULATION-79756/3.000000-98866',
-            'data/HNSCC-01-0188/08-13-2002-RT SIMULATION-59772/5.000000-29932',
-            'data/HNSCC-01-0175/06-04-2002-RT SIMULATION-40000/3.000000-91467'
-        ]
+    def __init__(self, transform=None):
         # Load the data file
-        TREATMENT_FILE = 'Patient-and-Treatment-Characteristics.xls'
-        treatment_data = pd.read_excel(TREATMENT_FILE)
-        # Get the valid dates and patients
-        valid_paths, valid_patients, valid_pixels = self.get_dates()
-        # Assign class attributes
-        treatment_data = treatment_data[treatment_data['TCIA code'].isin(valid_patients)]
+        self.treatment_data = pd.read_excel('Patient-and-Treatment-Characteristics.xls')
         # Convert TCIA codes to indices
-        self.all_patients = valid_patients #treatment_data['TCIA code'].tolist()
-        self.num_patients = len(valid_patients) #treatment_data.shape[0]
-        self.patient = torch.tensor(list(range(self.num_patients)))
+        self.all_patients = self.get_patients()
+        self.num_patients = len(self.all_patients)
+        self.patients = np.array(range(self.num_patients))
         # Set all the image file paths and image arrays
-        self.all_paths = valid_paths
-        self.all_axial = [self.crop_image(p,valid_pixels[i],'axial') for i,p in enumerate(self.all_paths)]
-        self.all_coronal = [self.crop_image(p,valid_pixels[i],'coronal') for i,p in enumerate(self.all_paths)]
-        self.all_sagittal = [self.crop_image(p,valid_pixels[i],'sagittal') for i,p in enumerate(self.all_paths)]
-        # Set Dead = 0 and Alive = 1
-        self.outcome = torch.tensor([0 if o=='Dead' else 1 for o in treatment_data['Alive or Dead'].tolist()])
+        self.all_axial = np.array([self.get_image(p,'axial') for p in self.all_patients])
+        self.all_coronal = np.array([self.get_image(p,'coronal') for p in self.all_patients])
+        self.all_sagittal = np.array([self.get_image(p,'sagittal') for p in self.all_patients])
+        # Set Dead = 1 and Alive = 0
+        self.outcome = np.array([1 if self.patient_outcome(p)=='Dead' else 0 for p in self.all_patients])
+        # Save the transform
+        self.transform = transform
 
     def __getitem__(self, index):
         """
         Return the TCIA code of the patient, the axial scan, coronal scan,
-        saggital scan, and their outcome (Dead = 0, Alive = 1).
+        sagittal scan, and their outcome (Dead = 1, Alive = 0).
+
         """
-        return (self.patient[index], self.all_axial[index],
-                self.all_coronal[index], self.all_sagittal[index],
-                self.outcome[index])
+        out_data = (self.patients[index], self.all_axial[index],
+                    self.all_coronal[index], self.all_sagittal[index],
+                    self.outcome[index])
+        if self.transform:
+            return self.transform(out_data)
+        return out_data
+
 
     def __len__(self):
+        """
+        """
         return self.num_patients
 
-    def get_dates(self):
-        valid_paths = []
-        valid_patients = []
-        valid_pixels = []
-        all_patients = os.listdir('data')
-        all_patients = [p for p in all_patients if '.DS_Store' not in p]
-        for p in all_patients:
-            patient_dir = os.path.join('data', p)
-            scan_dates = [d for d in os.listdir(patient_dir) if 'RT' in d]
-            scan_dates = [d for d in scan_dates if '.DS_Store' not in d]
-            # Some patients have multiple scans, the first should be the best
-            if len(scan_dates) > 1:
-                scan_dates = scan_dates[:1]
-            for s in scan_dates:
-                all_files = os.listdir(os.path.join('data', p, s))
-                all_files.sort()
-                ct_file = all_files[-1]
-                path = os.path.join('data', p, s, ct_file)
-                try:
-                    patient_dicom = self.load_scan(path)
-                    patient_pixels = self.get_pixels_hu(patient_dicom)
-                    valid_paths.append(path)
-                    valid_patients.append(p)
-                    valid_pixels.append(patient_pixels)
-                except:
-                    pass
-        return valid_paths, valid_patients, valid_pixels
+    def get_patients(self):
+        """
+        """
+        valid_patients = [f for f in os.listdir('images') if f.startswith('HNSCC')]
+        valid_patients.sort()
+        return valid_patients
 
-    def crop_image(self, path, patient_pixels, plane):
+    def patient_outcome(self, patient):
         """
-        Ideally remove this once the images are generated and put into the
-        data folder.
+        Get the outcome of a patient.
+
+        Returns
+        -------
+        patient_outcome : str
+            Either 'Alive' or 'Dead' based on the patient.
+
         """
-        if path in self.flip_array:
-            patient_pixels = patient_pixels[::-1]
-        # Get the middle/uppermost 128x128 patch
-        patch_size = 128
-        middle_slice_x = patient_pixels.shape[1]//2
-        start_ind_x = middle_slice_x - patch_size//2
-        stop_ind_x = middle_slice_x + patch_size//2
-        middle_slice_y = patient_pixels.shape[2]//2
-        start_ind_y = middle_slice_y - patch_size//2
-        stop_ind_y = middle_slice_y + patch_size//2
-        if patient_pixels.shape[0] > patch_size:
-            middle_slice_z = patient_pixels.shape[0] - patch_size//2
-            start_slice_z = patient_pixels.shape[0] - patch_size
+        # Assign class attributes
+        patient_outcome = self.treatment_data[self.treatment_data['TCIA code']==patient]['Alive or Dead'].values[0]
+        return patient_outcome
+
+    def get_image(self, patient, plane):
+        """
+        Parameters
+        ----------
+        patient : str
+        plane : str
+            Either 'axial', 'coronal', or 'sagittal'.
+
+        """
+        image_path = os.path.join('images', patient, plane+'.jpg')
+        image = io.imread(image_path, as_gray=True)
+        return image
+
+    def plot_image(self, images, batch_size=1):
+        """
+        Plot either a single image or batch of images.
+
+        Parameters
+        ----------
+        images : list
+        batch_size : int
+
+        """
+        images = images.numpy().transpose((0, 2, 3, 1))
+        fig = plt.figure(figsize=(8,8))
+        for i in range(batch_size):         
+            ax = plt.subplot(1,batch_size,i+1)
+            ax.axis('off')
+            plt.imshow(images[i], cmap=plt.get_cmap('bone'))
+        plt.tight_layout()
+        plt.show()
+
+
+class Rescale(object):
+    """
+    Rescale the image in a sample to a given size.
+
+    Parameters
+    ----------
+    output_size : tuple or int
+        Desired output size. If tuple, output is matched to output_size. If
+        int, smaller of image edges is matched to output_size keeping aspect
+        ratio the same.
+
+    """
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        (p, a, c, s, o) = sample
+        a = self.transform_image(a)
+        c = self.transform_image(c)
+        s = self.transform_image(s)
+        return (p, a, c, s, o)
+
+    def transform_image(self, image):
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
         else:
-            middle_slice_z = patient_pixels.shape[0]//2
-            start_slice_z = 0
-        # Get the correct image data based on the plane
-        if plane == 'axial':
-            img_data = patient_pixels[middle_slice_z,
-                                      start_ind_x:stop_ind_x,
-                                      start_ind_y:stop_ind_y]
-            img_data = np.pad(img_data,
-                              (((patch_size-img_data.shape[0])//2,
-                                (patch_size-img_data.shape[0])//2),
-                               ((patch_size-img_data.shape[1])//2,
-                                (patch_size-img_data.shape[1])//2)),
-                              'constant',
-                              constant_values=np.min(img_data))
-        if plane == 'coronal':
-            img_data = patient_pixels[start_slice_z:,
-                                      middle_slice_x,
-                                      start_ind_y:stop_ind_y]
-            img_data = np.pad(img_data,
-                              (((patch_size-img_data.shape[0])//2,
-                                (patch_size-img_data.shape[0])//2),
-                               ((patch_size-img_data.shape[1])//2,
-                                (patch_size-img_data.shape[1])//2)),
-                              'constant',
-                              constant_values=np.min(img_data))
-        if plane == 'sagittal':
-            img_data = patient_pixels[start_slice_z:,
-                                      start_ind_x:stop_ind_x,
-                                      middle_slice_y]
-            img_data = np.pad(img_data,
-                              (((patch_size-img_data.shape[0])//2,
-                                (patch_size-img_data.shape[0])//2),
-                               ((patch_size-img_data.shape[1])//2,
-                                (patch_size-img_data.shape[1])//2)),
-                              'constant',
-                              constant_values=np.min(img_data))
-        return torch.from_numpy(img_data)
+            new_h, new_w = self.output_size
+        new_h, new_w = int(new_h), int(new_w)
+        image = transform.resize(image, (new_h, new_w))
+        return image
 
-    def load_scan(self, path):
-        slices = [pydicom.dcmread(path + os.sep + s) for s in               
-                  os.listdir(path)]
-        slices = [s for s in slices if 'SliceLocation' in s]
-        slices.sort(key = lambda x: int(x.InstanceNumber))
-        try:
-            slice_thickness = np.abs(slices[0].ImagePositionPatient[2] -
-                                     slices[1].ImagePositionPatient[2])
-        except:
-            slice_thickness = np.abs(slices[0].SliceLocation -
-                                     slices[1].SliceLocation)
-        for s in slices:
-            s.SliceThickness = slice_thickness
-        return slices
 
-    def get_pixels_hu(self, scans):
-        image = np.stack([s.pixel_array for s in scans])
-        image = image.astype(np.int16)    
-        return np.array(image, dtype=np.int16)
+class ToTensor(object):
+    """
+    Convert numpy arrays to Tensors.
+
+    """
+    def __call__(self, sample):
+        (p, a, c, s, o) = sample
+        # Add a dimension for gray-scale images
+        a = np.expand_dims(a, axis=2)
+        c = np.expand_dims(c, axis=2)
+        s = np.expand_dims(s, axis=2)
+        # Swap color axis because:
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        a = a.transpose((2, 0, 1))
+        c = c.transpose((2, 0, 1))
+        s = s.transpose((2, 0, 1))
+        return (p, torch.from_numpy(a), torch.from_numpy(c),
+                torch.from_numpy(s), o)
 
 
 if __name__ == '__main__':
     batch_size = 5
-    dataset = SurvivalDataset()
-    data_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    dataset = SurvivalDataset(
+        transform=torchvision.transforms.Compose([Rescale(128), ToTensor()])
+    )
+    data_loader = DataLoader(dataset=dataset, batch_size=batch_size,
+                             shuffle=True, num_workers=2)
     data_iter = iter(data_loader)
+
+    fig = plt.figure(figsize=(8,8))
     (p, a, c, s, o) =  data_iter.next()
-
-    def imshow(tensor_img):
-        np_img = tensor_img.numpy()
-        plt.imshow(np.transpose(np_img, (1, 2, 0)))
-        plt.show()
-
-    imshow(torchvision.utils.make_grid(a, nrow=batch_size))
-    imshow(torchvision.utils.make_grid(c, nrow=batch_size))
-    imshow(torchvision.utils.make_grid(s, nrow=batch_size))
-    print(' '.join([str(o[j]) for j in range(batch_size)]))
-
+    a = a.numpy().transpose((0, 2, 3, 1))
+    c = c.numpy().transpose((0, 2, 3, 1))
+    s = s.numpy().transpose((0, 2, 3, 1))          
+    for i in range(batch_size):
+        print(i, a[i].shape, c[i].shape, s[i].shape, o)
+        ax = plt.subplot(batch_size,3,3*i+1)
+        ax.axis('off')
+        plt.imshow(a[i])
+        ax = plt.subplot(batch_size,3,3*i+2)
+        ax.axis('off')
+        plt.imshow(c[i])
+        ax = plt.subplot(batch_size,3,3*i+3)
+        ax.axis('off')
+        plt.imshow(s[i])
+    plt.tight_layout()
+    plt.show()
 
     # # Determine if they are alive or dead
     # alive_dead = [treatment_data[treatment_data['TCIA code'] == p]['Alive or Dead'].values[0] for p in valid_patients]
